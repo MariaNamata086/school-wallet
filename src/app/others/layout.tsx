3e4da7625bcf76340422fec54c409c63d8ab87d2
maria namata <namatamaria086@gmail.com>
2024-08-15 06:12:50 +0300

    Modify the styles on add student home page and restructure file structure to accommodate nested layouts

---
+++ src/app/others/layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import SharedNavBar from '../components/shared/SharedNavBar';
+
+function OtherPagesLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+  return (
+    <div>
+        <SharedNavBar/>
+        <div>{children}</div>
+    </div>
+  )
+}
+
+export default OtherPagesLayout
