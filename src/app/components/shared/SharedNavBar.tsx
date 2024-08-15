2b1f8ff24173acf430b25ab2d714e6ac088eeeab
maria namata <namatamaria086@gmail.com>
2024-08-15 18:33:09 +0300

    Modify shared navbar styling

--- src/app/components/shared/SharedNavBar.tsx
+++ src/app/components/shared/SharedNavBar.tsx
@@ -8,29 +8,29 @@ import user_icon from '@assets/user_icon_blue.png'
 
 function SharedNavBar() {
   return (
-    <nav className='flex flex-row items-center justify-between py-1 md:py-2 px-4 w-full bg-darkblue h-[110px]'>
-      <div className='flex items-center justify-around text-[15px] md:text-[20px] font-semibold'>
+    <nav className='flex flex-row items-center justify-between py-1 md:py-2 px-10 w-full bg-darkblue h-[58px] md:h-[80px]'>
+      <div className='flex items-center justify-around text-[15px] md:text-[20px] w-1/2 font-semibold'>
         <div className='w-[49px] h-[59.11px]'>
           <Image src={light_logo} alt="image of the project's light logo" />
         </div>
-        <span className='' style={{}}>
+        <Link href='/others' className='text-[18px]' style={{}}>
           Home
-        </span>
-        <span className='' style={{}}>
-          Transactions
-        </span>
-        <span className='' style={{}}>
-          Dashboard
-        </span>
-      </div>
-      <div className='flex place-items-centerjustify-around'>
-        <Link href='/notifications' className='w-[30px] h-[32px]'>
-          <FaRegBell />
         </Link>
-        <Link href='/notifications' className='w-[30px] h-[35px]'>
+        <Link href='/others/dashboard/transactions'className='text-[18px]' style={{}}>
+          Transactions
+        </Link>
+        <Link href='/others/dashboard' className='text-[18px]' style={{}}>
+          Dashboard
+        </Link>
+      </div>
+      <div className='flex items-center w-1/4 justify-around'>
+        <Link href='/notifications' className='w-[30px] h-[32px]'>
+          <FaRegBell size={35} />
+        </Link>
+        <Link href='/notifications' className='w-[30px] h-[35px] '>
           <Image src={settings_image} alt='image of settings icon' style={{objectFit:'contain'}}/>
         </Link>
-        <Link href='/notifications' className='w-[55.33px] h-[55.33px]'>
+        <Link href='/notifications' className='w-[45px] h-[45px]'>
          <Image src={user_icon} alt='image of a user icon' style={{objectFit:'contain'}}/>
         </Link>
       </div>
